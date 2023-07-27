83c3b96912e21a5e5e502eb7cfdf0fe5436a8f0d
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-07-27 16:26:45 -0300

    Fazendo a função de analise de resposta

--- Assets/scripts/FinalizaJogo.cs
+++ Assets/scripts/FinalizaJogo.cs
@@ -2,24 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class FinalizaJogo : MonoBehaviour
 {
        [SerializeField] private GameObject respostaJogo;
-       [SerializeField] Button[] opcoes;
+       [SerializeField] GameObject opcoes;
+       [SerializeField] GameObject interfaceAcertou;
+       [SerializeField] GameObject interfaceErrou;
+       Button[] botoes;
     // Start is called before the first frame update
     void Start()
     {
-        foreach(Button item in opcoes)
+        botoes = opcoes.GetComponentsInChildren<Button>();
+        foreach(Button item in botoes)
         {
-            item.onClick.AddListener(Teste);
-            Debug.Log( item.GetComponentInChildren<TMP_Text>().text);
+            item.onClick.AddListener(AnalisaResposta);
         }
     }
 
-    public void Teste(){
-        Debug.Log("oiii");
+    public void AnalisaResposta(){
+        GameObject objetoQueChamou = EventSystem.current.currentSelectedGameObject;
+        opcoes.SetActive(false);
+        if(objetoQueChamou.GetComponentInChildren<TMP_Text>().text=="teste1"){
+            interfaceAcertou.SetActive(true);
+        }else{
+            interfaceErrou.SetActive(true);
+        }
+
     }
     // Update is called once per frame
     void Update()
