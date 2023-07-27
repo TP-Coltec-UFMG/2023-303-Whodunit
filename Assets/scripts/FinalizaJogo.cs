9edc994fa3fb1e8b73241a96805cf54080dbbdf3
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-07-27 16:00:15 -0300

    criando opções e adicionando escutador de evento

--- Assets/scripts/FinalizaJogo.cs
+++ Assets/scripts/FinalizaJogo.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class FinalizaJogo : MonoBehaviour
 {
        [SerializeField] private GameObject respostaJogo;
+       [SerializeField] Button[] opcoes;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Button item in opcoes)
+        {
+            item.onClick.AddListener(Teste);
+            Debug.Log( item.GetComponentInChildren<TMP_Text>().text);
+        }
     }
 
+    public void Teste(){
+        Debug.Log("oiii");
+    }
     // Update is called once per frame
     void Update()
     {
@@ -24,4 +34,5 @@ public class FinalizaJogo : MonoBehaviour
     public void AtivaInterface(){
         respostaJogo.SetActive(true);
     }
+
 }
